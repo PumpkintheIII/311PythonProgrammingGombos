5fda6229ba70a5b1ebaa29303d8e2be0059d87e1
mgombos <50267286+PumpkintheIII@users.noreply.github.com>
2022-11-29 13:37:28 +0000

    Created step8.py Copied code from PLTW step 8, added multiline comment identifying parts of the code

---
+++ step8.py
@@ -0,0 +1,13 @@
+def text_prompt():
+  name = input("What is your name?")
+  print("Hello " + name)
+  
+text_prompt()
+
+"""
+function: def text_prompt():
+function call: text_prompt()
+variable: name
+strings: "What is your name?", "Hello"
+data types: The only data types are strings
+"""
