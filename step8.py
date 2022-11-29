aa4b1777aa362a068dcd173ebc08a66fad769d13
mgombos <50267286+PumpkintheIII@users.noreply.github.com>
2022-11-29 13:41:06 +0000

    commented the code from step8

--- step8.py
+++ step8.py
@@ -1,8 +1,13 @@
 def text_prompt():
+  #defines the text_prompt function
   name = input("What is your name?")
+  #asks for a user input to the question "What is your name?"
+  #intializes the name variable to the user input
   print("Hello " + name)
+  #prints "Hello" and the variable name in the console
   
 text_prompt()
+#calls the text_prompt function
 
 """
 function: def text_prompt():
