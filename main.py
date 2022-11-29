2728ca632c015ea4258835c52bc28b61fe6fee2f
mgombos <50267286+PumpkintheIII@users.noreply.github.com>
2022-11-29 13:31:26 +0000

    Steps 4-7 in PLTW, adds a multiline comment

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
+"""
+Max Gombos
+first_python_version1
+"""
+#^these two comments are from PLTW steps 4 and 5
 #PLTW CSE 3.1.1 Python Programmign Assignment
+
 #Steps 1-3 in PLTW:
 #runs the print command, prints the string to the console
 print("I am coding in Python!")
